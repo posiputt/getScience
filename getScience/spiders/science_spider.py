ef02de06ef90dc19369201af89dea2c7bff8b26e
Andrej Schoeke <github@andrej-schoeke.de>
2015-01-07 14:47:30 +0100

    Adjusted files with a test spider.

---
+++ getScience/spiders/science_spider.py
@@ -0,0 +1,15 @@
+import scrapy
+
+class ScienceSpider(scrapy.Spider):
+    name = "getScience"
+    allowed_domains = ["newscientist.com"]
+    start_urls = [
+                    "http://www.newscientist.com/special/instant-expert-general-relativity"
+                    "http://www.newscientist.com/special/carbon-capture-storage"
+                 ]
+
+    def parse(self, response):
+        filename = response.url.split("/")[-2]
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+
